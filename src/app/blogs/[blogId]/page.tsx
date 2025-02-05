d5021311713f34d63244d4455fa16d27d0de42fe
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-05 22:21:43 +0600

    feat: implement generateStaticParams in the BlogDetailsPage

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,4 +1,14 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+import { Blog } from "@/types";
+
+export const generateStaticParams = async () => {
+  const response = await fetch("http://localhost:5000/blogs");
+  const blogs = await response.json();
+
+  return blogs?.slice(0, 3)?.map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
 
 export default async function BlogDetailsPage({
   params,
